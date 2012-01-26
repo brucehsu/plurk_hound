eb451ba564df016dc253c305d8efd9f5848d1220
Bruce Hsu <brucehsu@gmail.com>
2012-01-26 19:38:03 +0800

    Response search implemented

--- hound.rb
+++ hound.rb
@@ -9,13 +9,23 @@ def search(keyword)
 		if f.end_with? '.html' 
 			fobj = File.open(File.join(plurks.path, f))
 			doc = Nokogiri::XML(fobj)
+
+			#Search plurks you posted
 			content = doc.xpath("//xmlns:div[@class='bigplurk']").inner_html.to_s
 			m = content.match(/<a.*>(.+)<\/a>\s*(<span.*>.*<\/span>)?\s*(.*)\s*<div class="meta">([0-9\-\ :]+),*.*<\/div>\s*/m)
 			original_author = m[1]
 			content = m[3]
 			datetime = m[4]
-			if content.include? keyword
-				result+="#{original_author}: #{content},#{datetime}<br />"
+			result += "#{original_author}: #{content},#{datetime}<br />" if content.include? keyword
+
+			#Search its responses
+			res = doc.xpath("//xmlns:div[@class='response']")
+			res.each do |r|
+				content = r.inner_html.to_s
+				m = content.match(/<a.*>(.+)<\/a>\s*(<span.*>.*<\/span>)?\s*<span class=\"plurk_content\">(.*)<\/span>.*/m)
+				responser = m[1]
+				content = m[3]
+				result += "#{responser}: #{content}<br />" if content.include? keyword
 			end
 		end
 	end
